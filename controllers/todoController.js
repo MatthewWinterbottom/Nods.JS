a4334cd5b8164736698b92a511e2a974d8e1df88
MattWint <mattwint911@gmail.com>
2019-11-16 23:43:50 +0000

    Have CRUD operations going to MongoDb

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,22 +1,46 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var data = [{item: 'Get milk'}, {item: 'Walk dog'}, {item: 'Pick up Grandmother'}];
+// Connect to the database
+
+mongoose.connect('mongodb+srv://test:test@todo-ws7lf.mongodb.net/test?retryWrites=true&w=majority')
+
+// Create scheme
+var todoSchema = new mongoose.Schema({
+    item: String
+})
+
+var Todo = mongoose.model('Todo', todoSchema);
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = (app) => {
     app.get('/todo', (req, res) => {
-        res.render('todo', {todos: data});
+
+        // Get data from mongo db and pass to the view
+
+        Todo.find({}, (err, data) => {
+            if (err) throw err;
+            res.render('todo', {todos: data});
+        });
     });
 
     app.post('/todo', urlencodedParser, (req, res) => {
-        data.push(req.body);
-        res.json(data);
+
+        // Get data from view and add it to mongo db
+
+        var newTodo = Todo(req.body).save((err, data) => {
+            if (err) throw err;
+            res.json(data);
+        })
     });
 
     app.delete('/todo/:item', (req, res) => {
-        data = data.filter(function(todo){
-            return todo.item.replace(/ /g, '-') !== req.params.item;
+
+        // Delete requested item from MongoDb
+
+        Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove((err, data) => {
+            if (err) throw err;
+            res.json(data);
         });
-        res.json(data);
     });
 }
